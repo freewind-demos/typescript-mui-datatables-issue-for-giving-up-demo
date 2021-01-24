194ceb4fba9c7d636b3629d12f4a2d91dd42c0b0
Freewind <nowindlee@gmail.com>
2021-01-25 01:12:33 +0800

    typescript-mui-datatables-issue-for-giving-up-demo

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-TypeScript React "mui-datatables" Expandable Demo
+TypeScript React "mui-datatables" Issue for Giving Up Demo
 ===================================
 
-某一行是否可以展开细节内容
+"mui-datatables" 存在一个严重的设计缺陷：
+
+在customBodyRenderer里没有办法拿到该行对应的原始对象，只能拿到转换后的数组，这给定制单元格内容带来极大的不便，
+因为我们必须要自己想办法根据单元格中的内容从另一个地方确定它所对应的对象，非常麻烦，甚至不太可能。
+
+具体讨论在这里：
+https://github.com/gregnb/mui-datatables/issues/1038
+
+让人难以理解的是，尽量提问者进行了详细的解释，并且大家也给出了各种必要性，但是作者很奇怪就是脑子转不过来弯，答非所问，
+最后给出来的理由是这种需求优先级低，自己时间不够，不想支持。看完就想放弃。
 
 ```
 npm install
 npm run demo
 ```
+
+在该demo里可以看到在自定义单元格中，没有user.id，只能拿到rowIndex，非常不方便。而且如果表格进行了排序，
+它还可能存在一个问题，就是rowIndex指的到底是排序前还是排序后的。
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
-import {TableCell, TableRow} from '@material-ui/core';
 
 type User = {
-  name: string, company: string, city: string, state: string;
+  id: number; name: string, company: string, city: string;
 }
 
 const columns: MUIDataTableColumn[] = [
   {name: "name", label: "Name", options: {filter: true, sort: true,}},
   {name: "company", label: "Company", options: {filter: true, sort: false,}},
-  {name: "city", label: "City", options: {filter: true, sort: false,}},
-  {name: "state", label: "State", options: {filter: true, sort: false,}},
+  {
+    name: "city", label: "City", options: {
+      filter: true, sort: false, customBodyRender: (value, tableMeta, updateValue) => {
+        return <pre>{JSON.stringify({value, tableMeta, updateValue}, null, 4)}</pre>
+      }
+    }
+  },
 ]
 
 const users: User[] = [
-  {name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY"},
-  {name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT"},
-  {name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL"},
-  {name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX"},
+  {id: 111, name: "AAA", company: "Test Corp 1", city: "Yonkers",},
+  {id: 222, name: "BBB", company: "Test Corp 2", city: "Hartford",},
+  {id: 333, name: "CCC", company: "Test Corp 3", city: "Tampa",},
+  {id: 444, name: "DDD", company: "Test Corp 3", city: "Dallas",},
 ]
 
 export default function Hello() {
@@ -25,35 +29,5 @@ export default function Hello() {
     title={"Employee List"}
     data={users}
     columns={columns}
-    options={{
-      filter: true,
-      filterType: 'dropdown',
-      responsive: 'standard',
-      expandableRows: true,
-      expandableRowsHeader: false,
-      expandableRowsOnClick: true,
-      isRowExpandable: (dataIndex, expandedRows) => {
-        console.log('### isRowExpandable', {dataIndex, expandedRows});
-        // if (dataIndex === 3 || dataIndex === 4) return false;
-        //
-        // // Prevent expand/collapse of any row if there are 4 rows expanded already (but allow those already expanded to be collapsed)
-        // if (expandedRows.data.length > 4 && expandedRows.data.filter(d => d.dataIndex === dataIndex).length === 0) return false;
-        return true;
-      },
-      rowsExpanded: [0, 1],
-      renderExpandableRow: (rowData, rowMeta) => {
-        const colSpan = rowData.length + 1;
-        return (
-          <TableRow>
-            <TableCell colSpan={colSpan}>
-              <div>Custom expandable row option.</div>
-              <div>rowData={JSON.stringify(rowData)}</div>
-              <div>rowMeta={JSON.stringify(rowMeta)}</div>
-            </TableCell>
-          </TableRow>
-        );
-      },
-      onRowExpansionChange: (curExpanded, allExpanded, rowsExpanded) => console.log(curExpanded, allExpanded, rowsExpanded)
-    }}
-      />
-    }
+  />
+}
